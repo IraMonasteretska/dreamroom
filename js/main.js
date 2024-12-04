5cc9496908b519f6d896737a3da6a8a663b845f8
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-12-04 18:20:26 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -39,8 +39,68 @@ $(document).ready(function () {
     });
 
     // upload
+    $(function () {
+        if ($("#slider").length > 0) {
+            $("#slider").slider({
+                min: 50,
+                max: 200,
+                value: 125,
+                slide: function (event, ui) {
+                    $(".image-box > img").css("transform", `scale(${ui.value / 100})`);
+                },
+            });
 
-  
+            $(".image-box").click(function () {
+                var previewImg = $(this).children("img");
+
+                $(this)
+                    .siblings()
+                    .children("input")
+                    .trigger("click");
+
+                $(this)
+                    .siblings()
+                    .children("input")
+                    .off("change")
+                    .change(function () {
+                        var reader = new FileReader();
+
+                        reader.onload = function (e) {
+                            var urll = e.target.result;
+                            $(previewImg).attr("src", urll);
+                            previewImg.parent().css("background", "transparent");
+                            previewImg.show();
+                            previewImg.siblings("p").hide();
+
+                            $(previewImg).siblings(".remove-btn").show();
+
+                            $("#slider").slider("value", 125);
+                            $(".image-box > img").css("transform", "scale(1)");
+                        };
+
+                        reader.readAsDataURL(this.files[0]);
+                    });
+            });
+
+            $(".remove-btn").click(function (event) {
+                event.stopPropagation();
+                var previewImg = $(this).siblings("img");
+                var inputField = $(this).closest(".control-group").find("input[type='file']");
+
+                previewImg.attr("src", "");
+                previewImg.parent().css("background", "");
+                previewImg.hide();
+                previewImg.siblings("p").show();
+
+                $(this).hide();
+
+                $("#slider").slider("value", 125);
+                $(".image-box > img").css("transform", "scale(1)");
+
+                inputField.val("");
+            });
+        }
+    });
 
 
 
@@ -51,5 +111,17 @@ $(document).ready(function () {
             this.scrollLeft += event.deltaY;
         });
     }
+
+    // fancybox
+    if ($("[data-fancybox]").length > 0) {
+        Fancybox.bind("[data-fancybox]", {
+            // Your custom options
+        });
+    }
+
+
+    // TEST
+    
+
 
 });
